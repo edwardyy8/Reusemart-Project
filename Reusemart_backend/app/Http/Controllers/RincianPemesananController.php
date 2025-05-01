2eb5b2d587400c903d751b53e10b291a402339cc
edwardyy8 <180895131+edwardyy8@users.noreply.github.com>
2025-05-01 20:58:48 +0700

    banyak hal, detailpenjualanpage, historypembelianpage

--- Reusemart_backend/app/Http/Controllers/RincianPemesananController.php
+++ Reusemart_backend/app/Http/Controllers/RincianPemesananController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rincian_Pemesanan;
 use Illuminate\Http\Request;
 
 class RincianPemesananController extends Controller
 {
     //
+
+    public function getPenjualanByIdPenitip($id)
+    {
+        try {
+            $penjualan = Rincian_Pemesanan::whereHas('barang', function ($query) use ($id) {
+                $query->where('id_penitip', $id);
+                })
+                ->with(['barang.fotoBarang'])
+                ->get();
+
+            if (!$penjualan) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Pemesanan tidak ditemukan',
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Penjualan',
+                'data' => $penjualan,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getPenjualanById($id)
+    {
+        try {
+            $penjualan = Rincian_Pemesanan::with(['barang.fotoBarang'])
+                ->where('id_rincianpemesanan', $id)
+                ->first();
+
+            if (!$penjualan) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Penjualan tidak ditemukan',
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Penjualan',
+                'data' => $penjualan,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
 }
